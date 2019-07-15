1fd81d18919e7e1df14c3f6d13fbd8fd3a255880
nettcod <nettcod@gmail.com>
2019-07-15 18:10:40 -0500

    Implemented new protocol

---
+++ PROTOCOL.markdown
@@ -0,0 +1,38 @@
+# Arduino Communications Standard
+## Interface
+Data transmissions occur via serial (UART) with a Baud rate of 115200.
+
+Checksums are calculated by taking the base 256 modulus of the sum of
+all bytes in the message following the checksum, including spacers.
+
+10 (0x0a) is not a valid command ID. This decision was made to prevent
+ambiguity with the start signal.
+
+## Format
+All numbers are given as hex.
+
+_      | Start   | Spacer | Checksum | Command | Spacer | Part 1 | Spacer | Part 2 |
+-------|---------|--------|----------|---------|--------|--------|--------|--------|
+Format |0a:0a:0a | 0d     | X        | X       | 0d     | X:X    | 0d     | X:X    |
+Bytes  |3        | 1      | 1        | 1       | 1      | 2      | 1      | 2      |
+
+Command | Name   | Part 1     | Part 2
+--------|--------|------------|-------
+1       | Left   | {VELOCITY} | Unused
+2       | Right  | {VELOCITY} | Unused
+3       | Height | {VELOCITY} | Unused
+4       | Dumper | {VELOCITY} | Unused
+5       | Digger | {VELOCITY) | Unused
+
+The velocities (including for the drive command) are provided as signed
+16 bit integers with the domain \[-1000..1000], with -1000 being full
+reverse and 1000 being full forward. Forwards indicates a forwards
+motion for the drive train, a motion towards dumping for the dumper and
+a motion towards digging for the digger.
+
+## Rationale
+This protocol was designed to carry a high volume of fixed-size data
+and have unambiguous message start signals. The presence of the spacers
+means that the start signal will always indicate the beginning of a
+message, unless a hardware fault results in the transmission of a faulty
+message, which the checksum will serve to mitigate.
--- RoboClaw_Driver/RoboClaw_Driver.ino
+++ RoboClaw_Driver/RoboClaw_Driver.ino
@@ -1,9 +1,8 @@
 /*
- * This program sends a PWM signal to one of three motor controllers based on a single-byte serial input. 
- * The input consists of 3 decimal digits:
- * The smallest digit (1-3) selects which device to output to.
- * The middle digit (0-9) determines the speed of the motor.
- * The largest digit (0 or 1) determines the drive direction of the motor.
+ * TODO: Implement checksum and error handling
+ * 
+ * This program sends a PWM signal to one of three motor controllers based on a 12-byte serial input. 
+ * The protocol is documented in PROTOCOL.markdown.
  *
  *  Created: April 27, 2019
  *  Author:  Cody Nettesheim
@@ -14,57 +13,58 @@
 #define CHANNEL_1 2
 #define CHANNEL_2 3
 #define CHANNEL_3 4
+#define CHANNEL_4 5
+#define CHANNEL_5 6
 //PWM parameters
-#define PULSE_RANGE    250 //Motor input has a range of 1500-250us to 1500+250us
-#define PULSE_NEUTRAL 1500 //Motor does not drive when the pulse width is 1500us
+#define PULSE_NEUTRAL 1500
+//Length of a message in bytes
+#define MESSAGE_LEN 12
 //Uncomment this line for debug mode:
-//#define DEBUG 1
+#define DEBUG 1
 
 //PWM objects for each motor
 Servo motor1;
 Servo motor2;
 Servo motor3;
+Servo motor4;
+Servo motor5;
 //Serial input data buffer
-uint8_t serial_byte  = 0;
-//Motor parameters set by serial input
-uint8_t drive_dir    = 0; //Direction of drive: 0 (forward) or 1 (backward)
-uint16_t drive_spd   = 0; //Speed of motor: 0 (stopped) - 9 (full speed)
-uint8_t motor_id     = 0; //Motor ID: selects motor 1, 2, or 3
+uint8_t serial_bytes[MESSAGE_LEN] = {0};
+//Motor parameters set by serial message
+int16_t drive_spd = 0; //Speed of motor: -1000 (full reverse) to +1000 (full forward)
+uint8_t motor_id  = 0; //Motor ID: selects motors 1-5
+
+
 
 void setup() {
     //Configure GPIO for each motor
     motor1.attach(CHANNEL_1);
     motor2.attach(CHANNEL_2);
     motor3.attach(CHANNEL_3);
+    motor4.attach(CHANNEL_4);
+    motor5.attach(CHANNEL_5);
     //Initialize Serial connection
-    Serial.begin(9600);
+    Serial.begin(115200);
     //Initially set motors to stopped
-    motor1.writeMicroseconds(1500);
-    motor2.writeMicroseconds(1500);
-    motor3.writeMicroseconds(1500);
+    motor1.writeMicroseconds(PULSE_NEUTRAL);
+    motor2.writeMicroseconds(PULSE_NEUTRAL);
+    motor3.writeMicroseconds(PULSE_NEUTRAL);
+    motor4.writeMicroseconds(PULSE_NEUTRAL);
+    motor5.writeMicroseconds(PULSE_NEUTRAL);
 }
 
 void loop() {
     //Wait for input
     if (Serial.available()) {
         //Get a single byte
-        serial_byte = Serial.read();
+        Serial.readBytes(serial_bytes, 12);
         //Least significant digit selects motor
-        motor_id = serial_byte % 10;
-        serial_byte /= 10;
+        motor_id = serial_bytes[5];
         //Middle digit sets motor speed
-        drive_spd = serial_byte % 10;
-        serial_byte /= 10;
-        //Most significant digit sets motor direction
-        drive_dir = serial_byte % 10;
+        drive_spd = (serial_bytes[7] << 8) & serial_bytes[8];
         //Convert drive speed to PWM output value
-        drive_spd *= PULSE_RANGE;
-        drive_spd /= 9;
-        if (drive_dir == 0) {
-            drive_spd = PULSE_NEUTRAL + drive_spd;
-        } else {
-            drive_spd = PULSE_NEUTRAL - drive_spd;
-        }
+        drive_spd /= 2;
+        drive_spd += PULSE_NEUTRAL;
         //Send PWM signal to selected output
         switch (motor_id) {
             case 1:
@@ -75,6 +75,12 @@ void loop() {
             break;
             case 3:
                 motor3.writeMicroseconds(drive_spd);
+            break;
+            case 4:
+                motor4.writeMicroseconds(drive_spd);
+            break;
+            case 5:
+                motor5.writeMicroseconds(drive_spd);
             break;
         };
         //Print debug output in debug mode
